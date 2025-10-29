e76bf04dc7ea94456a974301f769926c53b99ce8
BELLO OLAKUNLE <olakunlebello001@gmail.com>
2025-10-29 18:21:55 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js - Basic Service Worker
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
