65a4e9281a7c9a0415e0cf808d615d33c95498e9
BELLO OLAKUNLE <olakunlebello001@gmail.com>
2025-10-30 10:14:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-// sw.js - Basic Service Worker
-self.addEventListener('install', event => {
+// sw.js - Enhanced Service Worker
+const CACHE_NAME = 'gff-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
   console.log('Service Worker installed');
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
